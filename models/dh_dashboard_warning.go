89cc831bfc7d8ffd5766867cb3778d1b2681d273
dengjialong <dengjialong@datahunter.cn>
2018-01-11 16:36:50 +0800

    model字段调整

--- models/dh_dashboard_warning.go
+++ models/dh_dashboard_warning.go
@@ -2,18 +2,22 @@ package models
 
 import (
 	"time"
+
 	"github.com/astaxie/beego/orm"
 )
 
 type DhDashboardWarning struct {
 	DhBase
-	Id int64 `json:"-"`
-	ObjectId string `json:"_id"`
-	WidgetId string `json:"widget_id"`
-	Key string `json:"key"`
-	Compare string `json:"compare"`
-	Value int64 `json:"value"`
-	Status int `json:"status"`
+	Id         int64     `json:"-"`
+	ObjectId   string    `json:"_id"`
+	WidgetId   string    `json:"widget_id"`
+	UserId     string    `json:"user_id"`
+	Sql        string    `json:"sql"`
+	Field      string    `json:"field"`
+	Key        string    `json:"key"`
+	Compare    string    `json:"compare"`
+	Value      int64     `json:"value"`
+	Status     int       `json:"status"`
 	CreateTime time.Time `json:"-"`
 	UpdateTime time.Time `json:"-"`
 }
@@ -30,7 +34,7 @@ func (m *DhDashboardWarning) Query() orm.QuerySeter {
 	return m.query(m)
 }
 
-func (m *DhDashboardWarning) Save() bool{
+func (m *DhDashboardWarning) Save() bool {
 	if m.Id == 0 {
 		return m.create(m)
 	} else {
@@ -39,9 +43,9 @@ func (m *DhDashboardWarning) Save() bool{
 }
 
 func (m *DhDashboardWarning) Find(args ...interface{}) *DhDashboardWarning {
-	data := m.find(m,args...)
+	data := m.find(m, args...)
 	if data != nil {
-		_data,ok := data.(*DhDashboardWarning)
+		_data, ok := data.(*DhDashboardWarning)
 		if ok {
 			return _data
 		} else {
@@ -53,15 +57,15 @@ func (m *DhDashboardWarning) Find(args ...interface{}) *DhDashboardWarning {
 }
 
 func (m *DhDashboardWarning) Delete(args ...interface{}) bool {
-	return m.delete(m,args...)
+	return m.delete(m, args...)
 }
 
 func (m *DhDashboardWarning) SoftDelete(args ...interface{}) bool {
-	return m.softDelete(m,args...)
+	return m.softDelete(m, args...)
 }
 
 func (m *DhDashboardWarning) Count(filters map[string]interface{}) int64 {
-	return m.count(m,filters)
+	return m.count(m, filters)
 }
 
 func (m *DhDashboardWarning) List(filters map[string]interface{}) []*DhDashboardWarning {
@@ -86,22 +90,22 @@ func (m *DhDashboardWarning) OrderList(filters map[string]interface{}, order ...
 
 func (m *DhDashboardWarning) Pager(page int64, page_size int64, filters map[string]interface{}) (total int64, total_page int64, result []*DhDashboardWarning) {
 	var list []*DhDashboardWarning
-	total,total_page = m.pager(m, filters, page_size)
+	total, total_page = m.pager(m, filters, page_size)
 	_, err := m.pagerList(m, page, page_size, filters).All(&list)
 	if err != nil {
 		m.errReport(err)
-		return 0,0,nil
+		return 0, 0, nil
 	}
 	return total, total_page, list
 }
 
 func (m *DhDashboardWarning) OrderPager(page int64, page_size int64, filters map[string]interface{}, order ...string) (total int64, total_page int64, result []*DhDashboardWarning) {
 	var list []*DhDashboardWarning
-	total,total_page = m.pager(m, filters, page_size)
+	total, total_page = m.pager(m, filters, page_size)
 	_, err := m.pagerList(m, page, page_size, filters).OrderBy(order...).All(&list)
 	if err != nil {
 		m.errReport(err)
-		return 0,0,nil
+		return 0, 0, nil
 	}
 	return total, total_page, list
 }
