9683e92bc7e53116d8dc619fc621df76bec3f17a
dengjialong <dengjialong@datahunter.cn>
2018-01-23 11:27:38 +0800

    add mysql connecter

--- utils/jdbc.go
+++ utils/jdbc.go
@@ -35,3 +35,22 @@ func JDBC(sql string, db P) (result string, err error) {
 	logger.WithField("consume", milliseconds).Info("finish")
 	return
 }
+
+func JDBCToCSV(sql string, db P) (result string, err error) {
+	db_config := JsonEncode(db)
+	logger := JDBCLogger.WithFields(logrus.Fields{
+		"sql": sql,
+		"db":  db_config,
+	})
+	begin := time.Now()
+	logger.Info("begin")
+	result, err = HttpPost(def.Jdbc_proxy_url, nil, &P{"sql": sql, "db": db_config, "o": "csv"})
+	if err != nil {
+		logger.Error(err)
+	}
+	finish := time.Now()
+	nanoseconds := finish.Sub(begin).Nanoseconds()
+	milliseconds := fmt.Sprintf("%d.%d", nanoseconds/1e6, nanoseconds%1e6)
+	logger.WithField("consume", milliseconds).Info("finish")
+	return
+}
