71ea02bd6738c75628d4c551f744f0859852bcfd
dengjialong <dengjialong@datahunter.cn>
2018-02-09 12:21:12 +0800

    自定义日志不输出到控制台

--- utils/logger.go
+++ utils/logger.go
@@ -1,6 +1,10 @@
 package utils
 
 import (
+	"bufio"
+	"fmt"
+	"os"
+
 	"github.com/astaxie/beego"
 	"github.com/rifflock/lfshook"
 	"github.com/sirupsen/logrus"
@@ -23,6 +27,11 @@ func init() {
 
 func GetLogger(subject string) *logrus.Logger {
 	logger := logrus.New()
+	src, err := os.OpenFile(os.DevNull, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	if err != nil {
+		fmt.Println("err", err)
+	}
+	logger.Out = bufio.NewWriter(src)
 	logger.SetLevel(logrus.DebugLevel)
 	logger_root := beego.AppConfig.DefaultString("logger_path", "logs/")
 	pathMap := lfshook.PathMap{
